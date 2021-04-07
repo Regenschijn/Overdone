f2dd0b0e1465f31dcb536ff9a34938e155014baa
Jaap-Jan de Wit <jaap-jan@dodotech.dev>
2021-04-07 20:00:57 +0200

    update display name bouquet add resharper editor config

--- Items/BouquetBasic.cs
+++ Items/BouquetBasic.cs
@@ -5,62 +5,54 @@ using Terraria.ModLoader;
 using Terraria;
 using Terraria.DataStructures;
 
-namespace Overdone.Items
-{
-	public class BouquetBasic : ModItem
-	{
+namespace Overdone.Items {
+    public class BouquetBasic : ModItem {
 
-		private int counter;
+        private int _shootTimeoutCounter;
 
-		public override void SetStaticDefaults() 
-		{
-			// DisplayName.SetDefault("Bouquet"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
-			Tooltip.SetDefault("Ik ga deze bloemen helemaal op je schouders kapotslaan!");
-		}
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault("Flink bouquet");
+            Tooltip.SetDefault( "Ik ga deze bloemen helemaal op je schouders kapotslaan!" );
+        }
 
-		public override void SetDefaults() 
-		{
-			item.damage = 13;
-			item.magic = true;
-			item.width = 40;
-			item.height = 40;
-			item.useTime = 17;
-			item.useAnimation = 20;
-			item.useStyle = 1;
-			item.knockBack = 2;
-			item.value = 10000;
-			item.crit = 13;
-			item.rare = 1;
-			item.UseSound = SoundID.Item1;
-			item.autoReuse = true;
+        public override void SetDefaults() {
+            item.damage = 13;
+            item.magic = true;
+            item.width = 40;
+            item.height = 40;
+            item.useTime = 17;
+            item.useAnimation = 20;
+            item.useStyle = 1;
+            item.knockBack = 2;
+            item.value = 10000;
+            item.crit = 13;
+            item.rare = 1;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
 
-			item.shootSpeed = 10f;
-			item.shoot = ProjectileID.Leaf; 
+            item.shootSpeed = 10f;
+            item.shoot = ProjectileID.Leaf;
 
-		}
+        }
 
-		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-		{
-			counter++;
-			if (counter >= 4)
-			{
-				for (int i = 0; i < 7; i++)
-				{
-					Projectile.NewProjectile(position.X - 8f, position.Y + 8f, speedX + (float)Main.rand.Next(-130, 330) / 150f, speedY + (float)Main.rand.Next(-330, 130) / 150f, ProjectileID.Leaf, damage - 7, knockBack, ((Entity)player).whoAmI, 0f, 0f);
-				}
-				counter = 0;
-			}
-			return false;
-		}
+        public override bool Shoot( Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack ) {
+            _shootTimeoutCounter++;
+            if ( _shootTimeoutCounter < 4 ) return false;
+
+            for ( var i = 0; i < 7; i++ ) {
+                Projectile.NewProjectile( position.X - 8f, position.Y + 8f, speedX + ( float ) Main.rand.Next( -130, 330 ) / 150f, speedY + ( float ) Main.rand.Next( -330, 130 ) / 150f, ProjectileID.Leaf, damage - 7, knockBack, ( ( Entity ) player ).whoAmI, 0f, 0f );
+            }
+            _shootTimeoutCounter = 0;
+            return false;
+        }
 
 
-		public override void AddRecipes() 
-		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.DirtBlock, 10);
-			recipe.AddTile(TileID.WorkBenches);
-			recipe.SetResult(this);
-			recipe.AddRecipe();
-		}
-	}
+        public override void AddRecipes() {
+            var recipe = new ModRecipe( mod );
+            recipe.AddIngredient( ItemID.DirtBlock, 10 );
+            recipe.AddTile( TileID.WorkBenches );
+            recipe.SetResult( this );
+            recipe.AddRecipe();
+        }
+    }
 }
