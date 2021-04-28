1b79c1a95cee33d3e44c48ad01d51e6e200eebd6
DESKTOP-BP8GM0K\clement <clement.bezemer@gmail.com>
2021-04-28 14:37:07 +0200

    tried to fix projectile rotation, but not quite there yet. Also melee rotation is still bonkers.

--- Projectiles/SpearOfOlyndicusProjectile.cs
+++ Projectiles/SpearOfOlyndicusProjectile.cs
@@ -14,15 +14,20 @@ namespace Overdone.Projectiles //where it's stored, replace Mod with the name of
         {
             projectile.width = 2; //sprite is 2 pixels wide
             projectile.height = 20; //sprite is 20 pixels tall
-            projectile.aiStyle = 0; //projectile moves in a straight line
             projectile.friendly  = true; //player projectile
             projectile.ranged = true; //ranged projectile
-            projectile.timeLeft = 600; //lasts for 600 frames/ticks. Terraria runs at 60FPS, so it lasts 10 seconds.
+            projectile.timeLeft = 120; //lasts for 600 frames/ticks. Terraria runs at 60FPS, so it lasts 10 seconds.
+            projectile.aiStyle = 1;
+            projectile.penetrate = 5;
+
+
         }
 
         public override void AI()
         {
             Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 15, projectile.velocity.X * -0.5f, projectile.velocity.Y * -0.5f);   //spawns dust behind it, this is a spectral light blue dust
+            float velRotation = projectile.velocity.ToRotation();
+            projectile.rotation = velRotation + MathHelper.ToRadians( 90f );
         }
 
         public override void Kill(int timeLeft)
