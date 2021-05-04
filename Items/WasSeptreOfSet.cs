4c0cf8842b4242d621654dc7f90df18c837b5f72
DESKTOP-BP8GM0K\clement <clement.bezemer@gmail.com>
2021-05-04 22:11:05 +0200

    the other stuff

---
+++ Items/WasSeptreOfSet.cs
@@ -0,0 +1,85 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Overdone.Projectiles;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.DataStructures;
+
+namespace Overdone.Items {
+    public class WasSeptreOfSet : ModItem {
+        public override void SetStaticDefaults() {
+            DisplayName.SetDefault( "Set's Was Septre" );
+            Tooltip.SetDefault( "LMB: melee boink. RMB: Heavy havoc" );
+        }
+
+        public override void SetDefaults() {
+            item.melee = true;
+            item.width = 70;
+            item.height = 70;
+            item.value = Item.sellPrice( gold: 20 );
+            item.rare = ItemRarityID.Yellow;
+            item.noMelee = false;
+            item.noUseGraphic = false;
+            SetStabMode();
+        }
+
+        private void SetStabMode() {
+            item.melee = true;
+            item.magic = false;
+            item.damage = 23;
+            item.crit = 19;
+            item.mana = 0;
+            item.useTime = 33;
+            item.useAnimation = 33;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.knockBack = 13f;
+            item.UseSound = SoundID.DD2_MonkStaffSwing;
+            item.noMelee = false;
+            item.autoReuse = true;
+        }
+
+        private void SetThrowMode() {
+            item.melee = false;
+            item.magic = true;
+            item.damage = 35;
+            item.crit = 0;
+            item.mana = 1;
+            item.useTime = 18;
+            item.useAnimation = 18;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.knockBack = 5f;
+            item.UseSound = SoundID.DD2_FlameburstTowerShot;
+            item.shoot = ModContent.ProjectileType<AhnkProjectile>();
+            item.shootSpeed = 15f;
+            item.noMelee = true;
+            item.autoReuse = true;
+        }
+
+        public override bool AltFunctionUse( Player player ) => true;
+        public override bool CanUseItem( Player player ) {
+            if ( player.altFunctionUse == 2 ) { // Throw mode
+                SetThrowMode();
+            } else { // Stab mode
+                SetStabMode();
+            }
+            return true;
+        }
+
+        public override void OnHitNPC( Player player, NPC target, int damage, float knockBack, bool crit ) {
+            if ( player.altFunctionUse == 2 ) {
+                target.AddBuff( BuffID.Ichor, 90 );
+            }
+            else { }
+        }
+
+        public override void AddRecipes() {
+            var recipe = new ModRecipe( mod );
+            recipe.AddIngredient( ItemID.DirtBlock, 1 );
+            recipe.AddTile( TileID.WorkBenches );
+            recipe.SetResult( this );
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Projectiles/HarpeSwordHead.cs
@@ -0,0 +1,71 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Overdone;
+
+namespace Overdone.Projectiles {
+    public class HarpeSwordHead : ModProjectile {
+        public override void SetDefaults() {
+            projectile.Name = "Medusa Head";
+            projectile.width = 25;
+            projectile.height = 25;
+            projectile.aiStyle = 8;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 100;
+
+            projectile.hide = false;
+            projectile.ownerHitCheck = true;
+            projectile.ranged = true;
+            projectile.tileCollide = true;
+            projectile.friendly = true;
+            projectile.ignoreWater = false;
+
+        }
+
+        public override void AI() {
+            Player owner = Main.player[projectile.owner]; //Makes a player variable of owner set as the player using the projectile
+            projectile.light = 0.5f; 
+            projectile.rotation += (float)projectile.direction * 0.5f; 
+            int DustID = Dust.NewDust( new Vector2( projectile.position.X, projectile.position.Y + 2f ), projectile.width + 4, projectile.height + 4, 36, projectile.velocity.X * 0.2f, projectile.velocity.Y * 0.2f, 120, default( Color ), 0.75f ); //Spawns dust
+            Main.dust[DustID].noGravity = true; //Makes dust not fall
+            if ( projectile.timeLeft % 10 == 0 ) //”%” is modulate, which means the remainder, so this is saying that it will spawn projectiles every 15 frames
+            {
+                Projectile.NewProjectile( projectile.position.X, projectile.position.Y, MathHelper.Lerp( -1f, 1f, (float)Main.rand.NextDouble() ), MathHelper.Lerp( -1f, 1f, (float)Main.rand.NextDouble() ), mod.ProjectileType( "ExampleBulletB" ), 5 * (int)owner.rangedDamage, projectile.knockBack, Main.myPlayer ); //owner.rangedDamage is basically the damage multiplier for ranged weapons
+            }
+        }
+
+        //When you hit an NPC
+        public override void OnHitNPC( NPC n, int damage, float knockback, bool crit ) {
+            Player owner = Main.player[projectile.owner];
+            int rand = Main.rand.Next( 2 ); 
+            if ( rand == 0 ) {
+                n.AddBuff( 24, 180 ); //On Fire! debuff for 3 seconds
+            }
+            else if ( rand == 1 ) {
+                owner.statLife += 5; //Gives 5 Health
+                owner.HealEffect( 5, true ); //Shows you have healed by 5 health
+            }
+        }
+
+        //When the projectile hits a tile
+        public override bool OnTileCollide( Vector2 velocityChange ) {
+            if ( projectile.velocity.X != velocityChange.X ) {
+                projectile.velocity.X = -velocityChange.X / 2; //Goes in the opposite direction with half of its x velocity
+            }
+            if ( projectile.velocity.Y != velocityChange.Y ) {
+                projectile.velocity.Y = -velocityChange.Y / 2; //Goes in the opposite direction with half of its y velocity
+            }
+            return false;
+        }
+
+        //After the projectile is dead
+        public override void Kill( int timeLeft ) {
+            int rand = Main.rand.Next( 5 ); //Generates integers from 0 to 4
+            Projectile.NewProjectile( projectile.position.X, projectile.position.Y, 0, 0, 296, (int)(projectile.damage * 1.5), projectile.knockBack, Main.myPlayer ); // 296 is the explosion from the Inferno Fork
+        }
+    }
+}
+
