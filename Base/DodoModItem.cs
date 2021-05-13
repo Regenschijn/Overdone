6ead7a416cc7f442881f3100947e8e8272d5dfde
Jaap-Jan de Wit <jaap-jan@dodotech.dev>
2021-05-13 12:52:08 +0200

    add config for amount of hits before combo hit

--- Base/DodoModItem.cs
+++ Base/DodoModItem.cs
@@ -6,12 +6,22 @@ namespace Overdone.Base {
     public abstract class DodoModItem : ModItem {
         protected bool UseCombo = false;
         protected int ComboBuildPerHit = 1;
+        protected int HitsBeforeComboHit = 1;
+        private long _hitCount;
 
         public override void OnHitNPC( Player player, NPC target, int damage, float knockBack, bool crit ) {
             if ( UseCombo ) {
+                _hitCount++;
+                OnAddComboHit( _hitCount );
+            }
+            
+            base.OnHitNPC( player, target, damage, knockBack, crit );
+        }
+
+        protected virtual void OnAddComboHit( long currentHitCount ) {
+            if ( currentHitCount % HitsBeforeComboHit == 0 ) {
                 ComboManager.Add( ComboBuildPerHit );
             }
-            base.OnHitNPC( player, target, damage, knockBack, crit );
         }
     }
 }
--- Base/DodoModProjectile.cs
+++ Base/DodoModProjectile.cs
@@ -6,12 +6,22 @@ namespace Overdone.Base {
     public class DodoModProjectile : ModProjectile {
         protected bool UseCombo = false;
         protected int ComboBuildPerHit = 1;
+        protected int HitsBeforeComboHit = 1;
+        private long _hitCount;
 
-        public override void OnHitNPC( NPC target, int damage, float knockback, bool crit ) {
+        public override void OnHitNPC( NPC target, int damage, float knockBack, bool crit ) {
             if ( UseCombo ) {
+                _hitCount++;
+                OnAddComboHit( _hitCount );
+            }
+            
+            base.OnHitNPC( target, damage, knockBack, crit );
+        }
+
+        protected virtual void OnAddComboHit( long currentHitCount ) {
+            if ( currentHitCount % HitsBeforeComboHit == 0 ) {
                 ComboManager.Add( ComboBuildPerHit );
             }
-            base.OnHitNPC( target, damage, knockback, crit );
         }
     }
 }
