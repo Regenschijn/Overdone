44aabb8112fae33cd1dd39ab465eac3a1385587b
Jaap-Jan de Wit | DodoTech <jaap-jan@dodotech.dev>
2022-06-23 20:58:57 +0200

    fix buff compile error

--- Buffs/IrishFierceness.cs
+++ Buffs/IrishFierceness.cs
@@ -5,16 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria.ModLoader;
+using Terraria.ID;
 
 namespace Overdone.Buffs {
     class IrishFierceness : ModBuff {
         public override void SetStaticDefaults() {
             DisplayName.SetDefault( "Irish Fierceness" );
             Description.SetDefault( "Deal 10% more damage" );
-            canBeCleared/* tModPorter Note: Removed. Use BuffID.Sets.NurseCannotRemoveDebuff instead, and invert the logic */ = true;
+            BuffID.Sets.NurseCannotRemoveDebuff[Type] = false;
             Main.buffNoTimeDisplay[Type] = false;
             Main.debuff[Type] = false;
-
         }
 
         public override void Update( Player player, ref int buffIndex ) {
