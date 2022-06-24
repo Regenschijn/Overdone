ce67a2d1686a1cfac99b0b48cae331c41eb3f9e5
DESKTOP-BP8GM0K\clement <clement.bezemer@gmail.com>
2022-06-24 18:39:33 +0200

    added poisonFang data

---
+++ Projectiles/PoisonFang.cs
@@ -0,0 +1,31 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Overdone;
+using Overdone.Base;
+
+namespace Overdone.Projectiles {
+    public class PoisonFang : DodoModProjectile {
+        public override void SetDefaults() {
+            Projectile.width = 4;
+            Projectile.height = 20;
+            Projectile.aiStyle = 1;
+            Projectile.penetrate = 2;
+
+            Projectile.hide = false;
+            Projectile.ownerHitCheck = true;
+            Projectile.tileCollide = true;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Generic;
+        }
+
+        public override void OnHitNPC( NPC target, int damage, float knockback, bool crit ) {
+            base.OnHitNPC(target, damage, knockback, crit);
+            Player owner = Main.player[Projectile.owner];
+            target.AddBuff( 20, 660 );
+        }
+    }
+}
